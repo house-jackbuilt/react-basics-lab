aaf5ddf47895fcf561f51b5c98e645cf8f110698
Jack Pierce (20089478) <20089478@wit.ie>
2022-02-01 10:55:23 +0000

    Completed Week 2 Lab B

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,13 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import Task from './components/Task';
+import React, { useState } from 'react';
+import AddTaskForm from './components/Form';
 
 function App() {
+
+	const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
+      { id: 3, title: "Tidy up", deadline: "Today", done: false}
+    ]
+	
+  });
+
+  const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({tasks});
+    console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+  }
+  
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+	
   return (
-    <div className="App">
+    <div className="container">
       <h1>Tasky</h1>
+	  {taskState.tasks.map((task, index) => (              
+    <Task 
+      title={task.title}
+      description={task.description}
+      deadline={task.deadline}
+      key={task.id}
+	   done={task.done}
+      markDone={() => doneHandler(index)}
+	  deleteTask = {() => deleteHandler(index)}
+    />
+  ))}
+   <AddTaskForm />
     </div>
   );
+  
+ 
 }
 
 export default App;
--- tasky/src/components/task.js
+++ tasky/src/components/task.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const Task = () => {
+const Task = (props) => {
 
     return (
-        <p>This is a task!</p>
+         <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+             <p className="title">{props.title}</p>
+            <p>Due: {props.deadline}</p>
+			 <p>{props.children}</p>
+			 <button onClick={props.markDone} className='doneButton'>Done</button>
+			 <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
+        </div>
     )
+	
+	
 }
 
 export default Task;
-<Task title="Dishes" deadline="Today" />
